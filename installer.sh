5c3fe712a55c1cb880172f30d5bd2306d62bd07c
Yorchly <jorge.gutierrezvila@alum.uca.es>
2019-07-21 19:39:45 +0200

    [FIX] arreglos en installer.sh

--- installer.sh
+++ installer.sh
@@ -2,48 +2,73 @@
 echo "MENU"
 echo "1. Instalar sistemas con gestor de paquetes APT"
 echo "2. Instalar sistemas con gestor de paquete pacman"
+echo "Escribe la opcion: "
 read opcion
 convert=false
 if [ "$opcion" = 1 ]; then
-	echo "Instalando intérprete python 3"
+	echo "****INSTALANDO INTÉRPRETE PYTHON 3****"
 	sudo apt install python3
-	echo "Instalando youtube-dl desde repositorios"
+	echo ""
+	echo ""
+	echo "****INSTALANDO YOUTUBE-DL****"
 	sudo apt install youtube-dl -y
-	echo "Instalando pip3 desde repositorios"
+	echo ""
+	echo ""
+	echo "****INSTALANDO PIP 3****"
 	sudo apt install python3-pip -y
-	echo "Actualizando librerias de youtube-dl"
+	echo ""
+	echo ""
+	echo "****ACTUALIZANDO YOUTUBE-DL****"
 	sudo pip3 install --upgrade youtube-dl
-	echo "Instalando tkinter"
+	echo ""
+	echo ""
+	echo "****INSTALANDO TKINTER****"
 	sudo apt install python3-tk -y
-	echo "Instalando ffmpeg"
+	echo ""
+	echo ""
+	echo "****INSTALANDO FFMPEG****"
 	sudo apt install ffmpeg -y
+	echo ""
+	echo ""
 	clear
 	convert=true
 elif [ "$opcion" = 2 ]; then
-	echo "Instalando intérprete python 3"
+	echo "****INSTALANDO INTÉRPRETE PYTHON 3****"
 	sudo pacman -S python3
-	echo "Instalando pip3 desde repositorios"
+	echo ""
+	echo ""
+	echo "****INSTALANDO PIP 3****"
 	sudo pacman -S python-pip
-	echo "Instalando tkinter"
+	echo ""
+	echo ""
+	echo "****INSTALANDO TKINTER****"
 	sudo pacman -S tk
-	echo "Instalando youtube-dl"
+	echo ""
+	echo ""
+	echo "****INSTALANDO YOUTUBE-DL****"
 	sudo pacman -S youtube-dl
-	echo "Actualizando youtube-dl"
+	echo ""
+	echo ""
+	echo "****ACTUALIZANDO YOUTUBE-DL****"
 	sudo pip3 install --upgrade youtube-dl
-	echo "Instalando ffmpeg"
+	echo ""
+	echo ""
+	echo "****INSTALANDO FFMPEG****"
 	sudo pacman -S ffmpeg
+	echo ""
+	echo ""
 	convert=true	
 else
 	echo "No se ha reconocido la entrada, saliendo del script"
 fi
 
 if [ "$convert" = true ]; then
-	echo "Convirtiendo el fichero en una herramienta del sistema"
-	cp ytDownloader.py ytDownloader
-	chmod u+x ytDownloader
-	sudo mv ytDownloader /usr/local/bin
-	echo "ytDownloader se encuentra ahora en /usr/local/bin/"
-	echo "Para ejecutarla tan sólo tienes que abrir la terminal y escribir ytDownloader"
+	cp ytDownloader/ytDownloader.py ytDownloader/ytDownloader
+	chmod u+x ytDownloader/ytDownloader
+	sudo mv ytDownloader/ytDownloader /usr/local/bin
+	echo "####################################### FINALIZADO #######################################"
+	echo "Para ejecutar la aplicación, tan sólo tienes que abrir la terminal y escribir ytDownloader"
+	echo "##########################################################################################"
 fi
 
 
